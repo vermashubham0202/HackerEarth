f3d7cfdfe856639ac8e3ce194fcbaead6afdf997
Shubham Verma <vermashubham0202@gmail.com>
2023-05-31 19:01:57 +0530

    Best Index problem solved.

--- src/main/java/com/pidkui/easy/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.pidkui.easy;
-
-public class TestClass {
-    public static void main(String[] args) {
-        String str = "\"Hello\"";
-        str += ": null,";
-        System.out.println(str);
-    }
-}
